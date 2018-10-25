de2cb369a8b332a524b7e7023e44bfe2a0ddd298
Kate|Kateryna <32255660+phoneterrorist@users.noreply.github.com>
2018-10-26 00:05:01 +0300

    Create Driver.cs

---
+++ DriverManager/Driver.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication2
+{
+    class Driver
+    {
+        /*        ДАННЫЕ
+         *        - поддержка ОС
+         *        - тип устройства
+         */
+
+
+        private string driverName;
+        private int driverVersion;
+        private string driverType;
+
+        public Driver(int DriverVersion, string DriverName, string DriverType)
+        {
+            driverVersion = DriverVersion;
+            driverName = DriverName;
+            driverType = DriverType;
+        }
+
+        public int DriverVersion
+        {
+            get
+            {
+                return driverVersion;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Отрицательных значений не должно быть!");
+                }
+                else
+                    driverVersion = value;
+            }
+        }
+        
+        public string DriverName
+        {
+            get
+            {
+                return driverName;
+            }
+            set
+            {
+                if (value == " ")
+                {
+                    throw new ArgumentOutOfRangeException("Пустых строк не должно быть!");
+                }
+                else
+                    driverName = value;
+            }
+        }
+        
+        public override string ToString()
+        {
+              return "\nDriver: " + DriverName;
+        }
+
+        public bool Compare(Driver obj1, Driver obj2)
+        {
+             if (obj1.DriverVersion > obj2.DriverVersion)
+                return true;
+            return false;
+        }
+    }
+}
